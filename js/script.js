8bc290cdb7e8093c223df2dad311b19911a0b4a5
dana_c <conlindana@gmail.com>
2024-06-10 15:01:39 +0100

    created normalize.css file contents

--- js/script.js
+++ js/script.js
@@ -1,25 +1,9 @@
-const menuButton = document.querySelector("#toggleBtn");
-const menu = document.querySelector(".header-divider");
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  let toggleBtn = document.getElementById('toggleBtn');
-  var navbar = document.querySelector('.navbar');
-
-  toggleBtn.addEventListener('click', function() {
-    navbar.classList.toggle('active');
-    let expanded = toggleBtn.getAttribute('aria-expanded') === 'true' || false;
-    toggleBtn.setAttribute('aria-expanded', !expanded);
-  });
-
-});
-
-
-function myFunction() {
-  var x = document.getElementById("myLinks");
+function navFunction() {
+  var x = document.getElementById("navbar");
   if (x.style.display === "block") {
-    x.style.display = "none";
+  x.style.display = "none";
   } else {
-    x.style.display = "block";
+  x.style.display = "block";
   }
-}
+};

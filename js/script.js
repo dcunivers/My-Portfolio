d706c2e570ff7a142b835f43d47163474efc71b3
dana_c <conlindana@gmail.com>
2024-08-07 11:04:50 +0200

    added light to dark feature, altered projects

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
+const toggleBtn = document.querySelector('#toggleBtn')
+const shiftedHeroArea = document.querySelector('#hero-area')
 
+
+// Nav Bar Functions
 function navFunction() {
   const navmenu = document.getElementById("navbar");
     if (navmenu.style.display === "block") {
     navmenu.style.display = "none";
+    shiftedHeroArea.classList.remove('shifted')
     } else {
-  navmenu.style.display = "block";
+    navmenu.style.display = "block";
+    shiftedHeroArea.classList.add('shifted')
   }
 };
 
-const toggleBtn = document.querySelector('#toggleBtn')
-const toggleBtnIcon = document.querySelector()
-const dropDownMenu = document.querySelector()
 
-toggleBtn.onClick = function () {
-  dropDownMenu.classList.toggle('open')
-  const isOpen = dropDownMenu.classList.contains('open')
-}
+
+// Scroll Behavior page transition
+document.addEventListener('scroll', () => {
+  // total scrollable height of the document
+  const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+  // Get the current scroll position
+  const scrollTop = window.scrollY;
+  // Calculate the scroll percentage (0 to 1)
+  const scrollPercentage = scrollTop / scrollableHeight;
+  
+  // Interpolate the background color based on scroll percentage
+  const startColor = [236, 231, 225] // Light color
+  const endColor = [0, 0, 0]; // Dark color (black)
+  const currentColor = startColor.map((start, index) => 
+      Math.round(start + (endColor[index] - start) * scrollPercentage)
+  );
+
+  // Set the new background color
+  document.body.style.backgroundColor = `rgb(${currentColor.join(',')})`;
+});

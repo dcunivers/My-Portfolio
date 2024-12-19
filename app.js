04f2c0eee2fd05431e99f980415d50902acda200
dana_c <conlindana@gmail.com>
2024-12-19 18:12:20 -0500

    working on animations via react.js

---
+++ app.js
@@ -0,0 +1,19 @@
+import { animate, scroll } from motion;
+
+const projects = document.querySelectorAll(".project-item")
+
+animate(projects,
+  element,
+  { rotate: 90 },
+  { type: "spring", stiffness: 300 }
+);
+
+
+document.querySelectorAll("section > div").forEach((item) => {
+    scroll(animate(item, { opacity: [0, 1, 1, 0] }, { ease: "linear" }), {
+      target: item,
+      offset: ["start end", "end end", "start start", "end start"],
+    });
+  });
+
+  console.log();
